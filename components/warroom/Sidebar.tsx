c54ee5420041eccdb2ff4f4c1fd66b7e00fb7260
Matthew (ShirleyCRE) <matthew@sr-cre.com>
2026-04-09 12:43:04 -0500

    sidebar: change Under Contract icon from doc SVG to UC text label

--- components/warroom/Sidebar.tsx
+++ components/warroom/Sidebar.tsx
@@ -294,12 +294,7 @@ function PipeIcon() {
 }
 function DocIcon() {
   return (
-    <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.8">
-      <path d="M14 2H6a2 2 0 00-2 2v16a2 2 0 002 2h12a2 2 0 002-2V8z"/>
-      <polyline points="14 2 14 8 20 8"/>
-      <line x1="16" y1="13" x2="8" y2="13"/>
-      <line x1="16" y1="17" x2="8" y2="17"/>
-    </svg>
+    <span style={{ fontSize: 11, fontWeight: 900, lineHeight: 1, fontFamily: 'var(--font-body, system-ui, sans-serif)', letterSpacing: '-0.02em' }}>UC</span>
   )
 }
 function PeopleIcon() {
